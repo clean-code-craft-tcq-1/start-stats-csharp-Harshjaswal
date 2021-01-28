3cf34d8d6dfaff6e12013c0a8ff6694a7fe359e0
Harshjaswal <harshjaswal8@gmail.com>
2021-01-28 20:06:58 +0530

    Add files via upload

---
+++ Statistics/EmailAlert.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics
+{
+    public class EmailAlert : IAlerter
+    {
+        public bool emailSent = false;
+    }
+}
